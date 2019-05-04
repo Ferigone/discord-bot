41d705931288205eff6a13c41ddb2ed31e025852
Michał <32390670+Ferigone@users.noreply.github.com>
2019-05-04 13:46:00 +0200

    Added separated que for every server

--- commands/music.js
+++ commands/music.js
@@ -3,45 +3,56 @@ const ytdl = require("ytdl-core");
 const path = require("path");
 const scriptName = path.basename(__filename);
 const streamOptions = { seek: 0, volume: 1 };
-var que = [];
+var que = [{}];
 
 module.exports = () => {
   console.log("Loaded: " + scriptName);
 };
 
 module.exports.play = async (message, client) => {
+  let guildID = message.guild.id;
+  if (eval(`que[0][${guildID}]`) === undefined) {
+    eval(`que[0][${guildID}]=[]`);
+  }
+
+  // //Inside Varialbes
   let withoutPrefix = message.content.slice(config.arr[0].prefix.length);
   let split = withoutPrefix.split(/ +/);
   let ytLink = split[1];
   let vc = message.member.voiceChannelID;
   const channel = client.channels.get(vc);
+
+  // //If statements
   if (ytLink == undefined) {
     message.channel.send(":warning: Write YT Link :warning:");
     return;
   }
-  if (ytLink.includes("https://www.youtube.com/watch?v=")) {
-    if (que.length == 0) {
-      message.channel.fetchMessages({ limit: 1 }).then(messages => {
-        let lastMessage = messages.first();
-        lastMessage.delete();
-      });
-      que.push(ytLink);
-      playMusic(message, channel, que[0]);
-    } else if (que.length > 0) {
-      que.push(ytLink);
-      message.channel.send(":musical_note: Added To Que: " + que.length);
-    }
-  } else {
+  if (!ytLink.includes("https://www.youtube.com/watch?v=")) {
     message.channel.send(":warning: Write Correct YT Link :warning:");
+    return;
+  }
+  if (eval(`que[0][${guildID}].length`) == 0) {
+    message.channel.fetchMessages({ limit: 1 }).then(messages => {
+      let lastMessage = messages.first();
+      lastMessage.delete();
+    });
+    eval(`que[0][${guildID}].push('${ytLink}')`);
+    playMusic(message, channel, eval(`que[0][${guildID}][0]`), guildID);
+  } else if (eval(`que[0][${guildID}].length`) > 0) {
+    eval(`que[0][${guildID}].push('${ytLink}')`);
+    message.channel.send(
+      ":musical_note: Added To Que: " + eval(`que[0][${guildID}].length`)
+    );
   }
 };
 
-const playMusic = (m, h, y) => {
+const playMusic = (m, h, y, g) => {
   let message = m;
   let channel = h;
   let link = y;
+  let guildID = g;
   message.channel.send(`:musical_note: Actually playing:`);
-  message.channel.send(`:arrow_forward:  ${que[0]}`);
+  message.channel.send(`:arrow_forward:  ${eval(`que[0][${guildID}][0]`)}`);
   channel
     .join()
     .then(connection => {
@@ -50,9 +61,9 @@ const playMusic = (m, h, y) => {
       });
       const dispatcher = connection.playStream(stream, streamOptions);
       dispatcher.on("end", () => {
-        remove(que, link);
-        if (que[0] != undefined) {
-          playMusic(message, channel, que[0]);
+        remove(eval(`que[0][${guildID}]`), link);
+        if (eval(`que[0][${guildID}][0]`) != undefined) {
+          playMusic(message, channel, eval(`que[0][${guildID}][0]`));
         } else {
           if (que.length == 0) {
             setTimeout(() => {
